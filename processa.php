05332aead5c7fe8725813816f6944f14758be168
Gabriel758Eduardo <eduardosantos9406@gmail.com>
2020-08-27 01:19:47 -0300

    Projeto Finalizado

--- processa.php
+++ processa.php
@@ -1,3 +1,16 @@
 <?php
-
+	$u = $_POST["ultimo"];
+	$s = 0;
+	$t = 1;
+	
+	echo "Projeto sequencia de fibonacci da sequencia do 1 a sequencia do $u eh:
+	</br>";
+	
+	echo "0 </br> 1 </br>";
+		  
+	for ($i=1; $i<=$u-2; i++){
+		$t=$s + $t;
+		$s=$t - $s;
+		echo "$t. "</br>";
+	}
 ?>
