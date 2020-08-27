be5bdc783915251275722a7a341ef084045d412f
Gabriel758Eduardo <eduardosantos9406@gmail.com>
2020-08-27 01:17:41 -0300

    Projeto Finalizado

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title> Projeto - Sequencia de Fibonacci </title>
+	</head>
+	<body>
+		<form action="processa.php" method="POST">
+			<label> Digite um numero: </label>
+			<input type="number" name="ultimo">
+			
+			<input type="submit" value="Enviar" />
+			
+		</form>
+	</body>
 </html>
